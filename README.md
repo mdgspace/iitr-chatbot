a0ee6fc81b33fceb5d3fff7ad61541bc0de29846
Jitesh Jain <jitesh.jj2@gmail.com>
2020-09-30 12:22:56 +0530

    :zap: Add entity for programme and resolve the public model bug

--- README.md
+++ README.md
@@ -5,38 +5,44 @@
 - Update **pip**
 ```
 $ pip3 install -U pip
-
 ```
-
 - Setup the virtual environment
 ```
 $ python3 -m venv ./venv
 $ source ./venv/bin/activate
 ```
-
-- Install [RASA](https://rasa.com/docs/rasa/)
+- Install [RASA Open Source](https://rasa.com/docs/rasa/)
 ```
 $ pip3 install rasa==2.0.0rc2
 ```
-
 - Initialize a **rasa** project
+- The CLI will ask you before training an initial chatbot. **Don't train it for now**.
 ```
 $ mkdir rasa_bot
 $ cd rasa_bot
 $ rasa init
 ```
-The CLI will ask you before training an initial chatbot. Enter **y** and press *enter*.
+- Carry out any **one** of the following steps as a solution to this [issue](https://github.com/RasaHQ/rasa/issues/6806):
+
+    1. **First Method**: Install a public tokenizer
+
+        - Run `locate convert_tokenizer.py` and navigate to that location.
+        - Open `convert_tokenizer.py` and replace the existing `TF_HUB_MODULE_URL = (....)` with `TF_HUB_MODULE_URL = ("https://github.com/connorbrinton/polyai-models/releases/download/v1.0/model.tar.gz")` and save it.
+
+    2. **Second Method**: Use another tokenizer
+
+        - Open `config.yml` file in your `rasa_bot` directory and replace `ConveRTTokenizer` with `WhitespaceTokenizer`.
+        - Remove the `ConveRTFeaturizer` field.
 
 - Talking to your chatbot: Open another terminal window and run the command:
 ```
+$ rasa train
 $ rasa run actions
 ```
-This will setup a local server for the custom actions to be executed.
-
+- This will setup a local server for the custom actions to be executed.
 ```
 $ rasa shell 
 ```
-
-You can run `rasa shell nlu` to see the intent ranking and confidence.
+- You can run `rasa shell nlu` to see the intent ranking and confidence.
 
 **Happy Chatting!**
--- actions/actions.py
+++ actions/actions.py
@@ -12,7 +12,18 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-map_dept = {"CSE": "https://cse.iitr.ac.in/", "ECE": "https://cse.iitr.ac.in/", "EE": "https://ee.iitr.ac.in/", "Other": "https://www.iitr.ac.in/Main/pages/_en_Departments__en_.html"}
+map_dept = {"CSE": "https://cse.iitr.ac.in/", 
+            "ECE": "https://cse.iitr.ac.in/",
+            "EE": "https://ee.iitr.ac.in/",
+            "Other": "https://www.iitr.ac.in/Main/pages/_en_Departments__en_.html"}
+            
+map_programme = {"UG": "IITR provides UG courses in 17 branches.", 
+                "PG": "IITR provides M.Tech in 16 departments.",
+                "PHD": "IITR provides PHD 16 departments."}
+
+map_programme_dept = {"UG": "https://www.iitr.ac.in/academics/pages/Undergraduate_Programmes_Including_M_Sc__.html", 
+                    "PG": "https://www.iitr.ac.in/admissions/pages/Postgraduate.html",
+                    "PHD": "https://www.iitr.ac.in/admissions/pages/Phd.html"}
 
 class ActionDepartmentInfo(Action):
 
@@ -24,9 +35,26 @@ class ActionDepartmentInfo(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dept = tracker.slots.get("department_name")
         if dept in map_dept.keys():
-            output = "Please find more info about the {} department here: {}".format(dept,map_dept.get(dept))
+            output = "IITR has a {} department. For more information visit: {}".format(dept,map_dept.get(dept))
         else:
-            output = "Please find more info about the {} department here: {}".format(dept,map_dept.get("Other"))
+            output = "Please find informations about the departments here: {}".format(map_dept("Other"))
+        dispatcher.utter_message(text=output)
+
+        return []
+
+class ActionProgramme(Action):
+    
+    def name(self) -> Text:
+        return "action_programme"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        prg = tracker.slots.get("programme")
+        if prg in map_programme.keys():
+            output = "{}\nPlease find the various programmes available under {} here: {}.".format(map_programme.get(prg),prg,map_programme_dept.get(prg))
+        else:
+            output = "Please visit https://www.iitr.ac.in/admissions/pages/Freshers'_Special:_Main_Page.html for more information."
         dispatcher.utter_message(text=output)
 
         return []
